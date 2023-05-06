cfa924c3923ef87f8bcfa7b7c86f6ae06d7e615c
nurammarnaufal <nurammarnaufal@gmail.com>
2023-05-06 21:26:06 +0700

    v1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ msg: "success boss" });
+  res.json({ msg: "success boss", v: 1 });
 });
 
 app.post("/ulala", (req, res) => {
