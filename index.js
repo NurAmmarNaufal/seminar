01cb60af8873b1171c03a6b0a3fb6576c5ae4165
nurammarnaufal <nurammarnaufal@gmail.com>
2023-05-08 06:51:13 +0700

    add temp

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ msg: "success boss", v: 1 });
+  res.json({ msg: "success boss", v: 2 });
 });
 
 app.post("/ulala", (req, res) => {
