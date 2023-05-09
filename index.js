63c82aa2854c7479acabf16572af23edfb3a14be
nurammarnaufal <nurammarnaufal@gmail.com>
2023-05-09 22:18:08 +0700

    v4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ msg: "HI there 👋", v: 3 });
+  res.json({ msg: "HI there 👋", v: 4 });
 });
 
 app.post("/ulala", (req, res) => {
