db407d95fd28a819a73990e038d0f28541f6f7b0
nurammarnaufal <nurammarnaufal@gmail.com>
2023-05-20 10:04:52 +0700

    add limiter

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/httpreq", (req, res) => {
     tanah = 100;
   }
 
-  let kipas = mapRange(temp, 20, 35, 100, 255);
+  let kipas = mapRange(temp, 20, 35, 0, 255);
   let kipasListrik = mapRange(temp, 20, 35, 0, 3);
   let kelembapanTanah = mapRange(tanah, 0, 100, 20, 0);
 
